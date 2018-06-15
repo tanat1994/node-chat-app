a9f3ee2f4af3ceca303ac3fc59e65c57d1592b18
tanat1994 <tanat1994@gmail.com>
2018-06-15 15:17:20 +0700

    Add timestamp

---
+++ playgrounds/time.js
@@ -0,0 +1,9 @@
+
+// Jan 1 st 1970 00.00.00 am
+
+// const time = new Date();
+// console.log(time.getMonth()+1);
+
+const moment = require('moment');
+const date = moment();
+console.log(date.format('MMM Do, YYYY'));
